844d945f875d7c7f3b5905e0261b3e580b642796
MihirKotecha <mihir.k1@ahduni.edu.in>
2024-12-23 23:26:08 +0530

    Import Seed Page

--- src/pages/ImportSeed.tsx
+++ src/pages/ImportSeed.tsx
@@ -10,17 +10,19 @@ const ImportSeed = () => {
     }
 
     return (
-        <div className="bg-slate-700 h-screen w-screen flex justify-center items-center">
-            <div className="flex justify-center items-center w-full">
-                <div className="bg-slate-400 rounded-lg w-3/4 flex justify-center items-center flex-col py-10">
-                    <h1 className="text-center text-2xl font-serif pb-4">Import Seed</h1>
+        <div className="bg-black h-screen w-screen flex justify-center items-center">
+            <div className="flex justify-center items-center w-full h-full">
+                <div className="bg-[#323232] rounded-lg w-3/4 flex justify-center items-center flex-col py-10 h-3/4">
+                    <div className="w-full flex px-24">
+                        <h1 className="text-center text-3xl font-serif pb-4 text-white flex justify-start ">Import Seed</h1>
+                    </div>
                     <div className="grid grid-cols-3 place-items-center w-full">
                         {
                             seed.map((value, index) => (
                                 <input key={index} value={value} type="text" onChange={(e) => handleChange(index, e.target.value)} className="w-1/2 p-2 m-2 rounded-lg" placeholder={`${index + 1}`} />
                             ))
                         }
-                        <button className="w-3/4 col-span-3  p-2 mt-8 bg-slate-700 text-white rounded-lg">Import</button>
+                        <button className="w-3/4 col-span-3  p-2 mt-8 bg-red-600 text-white rounded-full text-lg">Import</button>
                     </div>
                 </div>
             </div>
